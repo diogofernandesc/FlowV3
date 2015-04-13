dd3a6637a73a908a35426f28d7387380497985da
Diogo <diogofernandescon@gmail.com>
2015-04-13 18:49:20 +0100

    Update Create.py

--- Src/Main/Create.py
+++ Src/Main/Create.py
@@ -13,7 +13,8 @@ def text_objects(text, font):
     return textSurface, textSurface.get_rect()
 
 
-def button(msg, button_x, button_y, button_w, button_h, icolour, acolour, fontsize, action= None): # icolour = inactive colour & acolour = active colour
+def button(msg, button_x, button_y, button_w, button_h, icolour, acolour, fontsize, action= None):
+    # icolour = inactive colour & acolour = active colour
     global nresets
     global current_level
 
@@ -29,7 +30,9 @@ def button(msg, button_x, button_y, button_w, button_h, icolour, acolour, fontsi
         if click[0] == 1 and action!= None:
             if action == "level 1":
                 time.sleep(0.2)
-                Main.game_level("level 1", "level 2", "reset_level1", 2, 2, 4, 4, 5, 4, 3, 5, 3, 3, 1, 4, 5, 0, 3, 4, 1, 2, 5, 3, 5, 0, 3, 4, 2, 2, 4, 4, 1, 2, 5, 3, 3, 3, 1, 4, 5, 4, 3, 5, 6, 28, 15, 29, 14, 24, 22, 26, 30, 34)
+                Main.game_level("level 1", "level 2", "reset_level1", 2, 2, 4, 4, 5, 4, 3, 5, 3, 3, 1, 4,
+                5, 0, 3, 4, 1, 2, 5, 3, 5, 0, 3, 4, 2, 2, 4, 4, 1, 2, 5, 3, 3, 3, 1, 4, 5, 4, 3, 5, 6, 28, 
+                15, 29, 14, 24, 22, 26, 30, 34)
             
             elif action == "instructions":
                 Main.instruction_menu()
@@ -48,7 +51,9 @@ def button(msg, button_x, button_y, button_w, button_h, icolour, acolour, fontsi
                     reset_text = reset_font.render("No more resets available", True, Main.Red)
                     Main.screen.blit(reset_text,(5,620))
                 else:
-                    Main.game_level("level 1", "level 2", "reset_level1", 2, 2, 4, 4, 5, 4, 3, 5, 3, 3, 1, 4, 5, 0, 3, 4, 1, 2, 5, 3, 5, 0, 3, 4, 2, 2, 4, 4, 1, 2, 5, 3, 3, 3, 1, 4, 5, 4, 3, 5, 6, 28, 15, 29, 14, 24, 22, 26, 30, 34)
+                    Main.game_level("level 1", "level 2", "reset_level1", 2, 2, 4, 4, 5, 4, 3, 5, 3, 3, 1, 4, 
+                    5, 0, 3, 4, 1, 2, 5, 3, 5, 0, 3, 4, 2, 2, 4, 4, 1, 2, 5, 3, 3, 3, 1, 4, 5, 
+                    4, 3, 5, 6, 28, 15, 29, 14, 24, 22, 26, 30, 34)
 
                     
             elif action == "reset_level2":
@@ -57,7 +62,9 @@ def button(msg, button_x, button_y, button_w, button_h, icolour, acolour, fontsi
                     reset_text = reset_font.render("No more resets available", True, Main.Red)
                     Main.screen.blit(reset_text,(5,620))
                 else:
-                    Main.game_level("level 2", "level 3", "reset_level2", 0, 1, 5, 3, 1, 1, 5, 4, 3, 1, 4, 4, 1, 3, 0, 5, 1, 2, 1, 4, 1, 3, 0, 5, 0, 1, 5, 3, 1, 2, 1, 4, 3, 1, 4, 4, 1, 1, 5, 4, 20, 31, 7, 24, 14, 26, 10, 29, 8, 30)
+                    Main.game_level("level 2", "level 3", "reset_level2", 0, 1, 5, 3, 1, 1, 5, 4, 3, 1, 4,
+                    4, 1, 3, 0, 5, 1, 2, 1, 4, 1, 3, 0, 5, 0, 1, 5, 3, 1, 2, 1, 4, 3, 1, 4, 4, 1, 
+                    1, 5, 4, 20, 31, 7, 24, 14, 26, 10, 29, 8, 30)
             
             elif action == "reset_level3":
                 if Main.nresets == -1:
@@ -65,7 +72,9 @@ def button(msg, button_x, button_y, button_w, button_h, icolour, acolour, fontsi
                     reset_text = reset_font.render("No more resets available", True, Main.Red)
                     Main.screen.blit(reset_text,(5,620))
                 else:
-                    Main.game_level("level 3", "level 4", "reset_level3", 4, 1, 4, 4, 4, 2, 4, 5, 5, 1, 3, 5, 0, 0, 1, 4, 0, 1, 2, 5, 0, 0, 1, 4, 4, 1, 4, 4, 0, 1, 1, 4, 5, 1, 3, 5, 4, 2, 4, 5, 1, 26, 11, 29, 7, 33, 12, 34, 17, 35)
+                    Main.game_level("level 3", "level 4", "reset_level3", 4, 1, 4, 4, 4, 2, 4, 5, 5, 1, 3, 
+                    5, 0, 0, 1, 4, 0, 1, 2, 5, 0, 0, 1, 4, 4, 1, 4, 4, 0, 1, 1, 4, 5, 1, 3, 5, 4, 2, 4, 
+                    5, 1, 26, 11, 29, 7, 33, 12, 34, 17, 35)
                     
             elif action == "reset_level4":
                 if Main.nresets == -1:
@@ -73,7 +82,9 @@ def button(msg, button_x, button_y, button_w, button_h, icolour, acolour, fontsi
                     reset_text = reset_font.render("No more resets available", True, Main.Red)
                     Main.screen.blit(reset_text,(5,620))
                 else:
-                    Main.game_level("level 4", "level 5", "reset_level4", 5, 0, 0, 4, 0, 0, 3, 4, 1, 2, 2, 3, 0, 2, 2, 4, 0, 1, 3, 3, 0, 2, 2, 4, 5, 0, 0, 4, 0, 1, 3, 3, 1, 2, 2, 3, 0, 0, 3, 4, 13, 27, 6, 25, 7, 22, 14, 21, 1, 28)
+                    Main.game_level("level 4", "level 5", "reset_level4", 5, 0, 0, 4, 0, 0, 3, 4, 1, 2, 2, 
+                    3, 0, 2, 2, 4, 0, 1, 3, 3, 0, 2, 2, 4, 5, 0, 0, 4, 0, 1, 3, 3, 1, 2, 2, 3, 0, 0, 3, 4, 13, 
+                    27, 6, 25, 7, 22, 14, 21, 1, 28)
 
             elif action == "reset_level5":
                 if Main.nresets == -1:
@@ -81,24 +92,34 @@ def button(msg, button_x, button_y, button_w, button_h, icolour, acolour, fontsi
                     reset_text = reset_font.render("No more resets available", True, Main.Red)
                     Main.screen.blit(reset_text,(5,620))
                 else:
-                    Main.game_level("level 5", "game complete", "reset_level5", 1, 2, 3, 2, 2, 4, 4, 4, 1, 1, 2, 5, 5, 0, 0, 5, 5, 1, 3, 5, 5, 0, 0, 5, 1, 2, 3, 2, 5, 1, 3, 5, 1, 1, 2, 5, 2, 4, 4, 4, 6, 31, 14, 16, 12, 34, 8, 33, 27, 29)
+                    Main.game_level("level 5", "game complete", "reset_level5", 1, 2, 3, 2, 2, 4, 4, 4, 1, 1, 
+                    2, 5, 5, 0, 0, 5, 5, 1, 3, 5, 5, 0, 0, 5, 1, 2, 3, 2, 5, 1, 3, 5, 1, 1, 2, 5, 2, 4, 4, 4, 6, 31, 14,
+                    16, 12, 34, 8, 33, 27, 29)
 
                 
             elif action == "level 2":
                 time.sleep(0.2)
-                Main.game_level("level 2", "level 3", "reset_level2", 0, 1, 5, 3, 1, 1, 5, 4, 3, 1, 4, 4, 1, 3, 0, 5, 1, 2, 1, 4, 1, 3, 0, 5, 0, 1, 5, 3, 1, 2, 1, 4, 3, 1, 4, 4, 1, 1, 5, 4, 20, 31, 7, 24, 14, 26, 10, 29, 8, 30)
+                Main.game_level("level 2", "level 3", "reset_level2", 0, 1, 5, 3, 1, 1, 5, 4, 3, 1, 4, 4, 1, 3, 0, 5, 
+                1, 2, 1, 4, 1, 3, 0, 5, 0, 1, 5, 3, 1, 2, 1, 4, 3, 1, 4, 4, 1, 1, 5, 4, 20, 31, 7, 24, 
+                14, 26, 10, 29, 8, 30)
 
             elif action == "level 3":
                 time.sleep(0.2)
-                Main.game_level("level 3", "level 4", "reset_level3", 4, 1, 4, 4, 4, 2, 4, 5, 5, 1, 3, 5, 0, 0, 1, 4, 0, 1, 2, 5, 0, 0, 1, 4, 4, 1, 4, 4, 0, 1, 1, 4, 5, 1, 3, 5, 4, 2, 4, 5, 1, 26, 11, 29, 7, 33, 12, 34, 17, 35)
+                Main.game_level("level 3", "level 4", "reset_level3", 4, 1, 4, 4, 4, 2, 4, 5, 5, 1, 3, 
+                5, 0, 0, 1, 4, 0, 1, 2, 5, 0, 0, 1, 4, 4, 1, 4, 4, 0, 1, 1, 4, 5, 1, 3, 5, 4, 2, 4, 5, 1, 26, 11,
+                29, 7, 33, 12, 34, 17, 35)
     
             elif action == "level 4":
                 time.sleep(0.2)
-                Main.game_level("level 4", "level 5", "reset_level4", 5, 0, 0, 4, 0, 0, 3, 4, 1, 2, 2, 3, 0, 2, 2, 4, 0, 1, 3, 3, 0, 2, 2, 4, 5, 0, 0, 4, 0, 1, 3, 3, 1, 2, 2, 3, 0, 0, 3, 4, 13, 27, 6, 25, 7, 22, 14, 21, 1, 28)
+                Main.game_level("level 4", "level 5", "reset_level4", 5, 0, 0, 4, 0, 0, 3, 4, 1, 2, 2, 
+                3, 0, 2, 2, 4, 0, 1, 3, 3, 0, 2, 2, 4, 5, 0, 0, 4, 0, 1, 3, 3, 1, 2, 2, 3, 0, 0, 3, 4, 13, 
+                27, 6, 25, 7, 22, 14, 21, 1, 28)
             
             elif action == "level 5":
                 time.sleep(0.2)
-                Main.game_level("level 5", "game complete", "reset_level5", 1, 2, 3, 2, 2, 4, 4, 4, 1, 1, 2, 5, 5, 0, 0, 5, 5, 1, 3, 5, 5, 0, 0, 5, 1, 2, 3, 2, 5, 1, 3, 5, 1, 1, 2, 5, 2, 4, 4, 4, 6, 31, 14, 16, 12, 34, 8, 33, 27, 29)
+                Main.game_level("level 5", "game complete", "reset_level5", 1, 2, 3, 2, 2, 4, 4, 4, 1, 1, 
+                2, 5, 5, 0, 0, 5, 5, 1, 3, 5, 5, 0, 0, 5, 1, 2, 3, 2, 5, 1, 3, 5, 1, 1, 2, 5, 2, 4, 4, 4, 6, 
+                31, 14, 16, 12, 34, 8, 33, 27, 29)
 
             elif action == "game complete":
                 LevelCompletion.situation("game complete", action, current_level)
@@ -112,7 +133,7 @@ def button(msg, button_x, button_y, button_w, button_h, icolour, acolour, fontsi
     else:
         Main.pygame.draw.rect(Main.screen, icolour, (button_x,button_y,button_w,button_h))
     
-    smallText = Main.pygame.font.Font("freesansbold.ttf", fontsize)
-    textSurf, textRect = text_objects(msg, smallText)
+    buttonText = Main.pygame.font.Font("freesansbold.ttf", fontsize)
+    textSurf, textRect = text_objects(msg, buttonText)
     textRect.center = ((button_x+(button_w/2),(button_y+(100/2))))
     Main.screen.blit(textSurf, textRect)
