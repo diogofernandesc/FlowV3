faa6cdbb4b65e9991664231fe29e06435c25c1aa
Diogo <diogofernandescon@gmail.com>
2015-04-14 14:39:39 +0100

    Update __init__.py

--- Src/Main/__init__.py
+++ Src/Main/__init__.py
@@ -7,7 +7,6 @@ import time
 
 from Main import Grid, CircleRender, GridPosition, Connection, CheckPosition, Create, Time, LevelCompletion, Reset, Line
 
-
 Black = (0 , 0, 0)
 White = (255, 255, 255)
 Blue = (0 , 0, 255)
@@ -25,7 +24,6 @@ Grey = (150, 150, 150)
 DarkGrey = (110, 110, 100)
 VeryDarkGrey = (20, 20, 20)
 
-
 # Initialize pygame imported modules
 pygame.init()
 
@@ -33,7 +31,6 @@ display_width = 600
 display_height = 800
 scr_size = (display_width, display_height)
 screen = pygame.display.set_mode(scr_size)
-
 clock = pygame.time.Clock()
 
 # Loops until the user clicks the close Button
@@ -52,7 +49,6 @@ grid_position = ()
 # Defining grid positions for circles
 # Array storing different grid positions possible for circles (their center) for both x and y:
 
-
 '''
 Position in pixels: 
 Position 0: 50
@@ -64,7 +60,6 @@ position 5: 550
 '''
 
 grid = [50, 150, 250, 350, 450, 550]
-
 
 pygame.init()
 
@@ -96,9 +91,6 @@ YellowLink = False
 GreenLink = False
 BlueLink = False
 
-
-                                
-
 def instruction_menu():
     pygame.display.set_caption("Flow Instructions")
     instrMenu = True
@@ -121,9 +113,7 @@ def instruction_menu():
         Create.text("- Enjoy yourself!", 300,500, 15)
         Create.button("Play", 50,600,200,100, DarkGreen, Green,30, "level 1")
         Create.button("Quit",350,600,200,100, DarkRed, Red,30, "quit")
-        
         pygame.display.update()
-
 
 def game_intro():
     pygame.display.set_caption("Flow Main Menu")
@@ -143,11 +133,8 @@ def game_intro():
         Create.button("Play", 200,300,200,100, DarkGreen, Green,30, "level 1")
         Create.button("Instructions",200,450,200,100, DarkBlue, Blue, 30, "instructions")
         Create.button("Quit",200,600,200,100, DarkRed, Red, 30, "quit")
-        
-        
         pygame.display.update()
         
-
 def game_level(current_level, action, rAction, R1x, R1y, R2x, R2y, B1x, B1y, B2x, B2y, G1x, G1y, 
                 G2x, G2y, O1x, O1y, O2x, O2y, Y1x, Y1y, Y2x, Y2y,o_x1, o_y1, o_x2, o_y2, r_x1, r_y1, r_x2, r_y2, 
                 yel_x1, yel_y1, yel_x2, yel_y2, g_x1, g_y1, g_x2, g_y2, b_x1, b_y1, b_x2, b_y2,
@@ -183,7 +170,6 @@ def game_level(current_level, action, rAction, R1x, R1y, R2x, R2y, B1x, B1y, B2x
     
     # Naming the caption of the window opened for the Game
     pygame.display.set_caption("Flow " + current_level)
-    
     
     frame_count = 0
     frame_rate = 60
@@ -237,19 +223,11 @@ def game_level(current_level, action, rAction, R1x, R1y, R2x, R2y, B1x, B1y, B2x
     circle_list.add(RedCircle1, RedCircle2, BlueCircle1, BlueCircle2, GreenCircle1, GreenCircle2,
     OrangeCircle1, OrangeCircle2, YellowCircle1, YellowCircle2)
     
-    
     # Grid built here:
     Grid.build_grid()
 
     Reset.counter(nresets)
     Connection.complete_status()
-    
-    '''# Default value for line colour
-    line_colour = ()
-    
-    # Default value for grid position
-    grid_position = ()'''
-    
     
     # Condition variables for links
     OrangeLink = False
@@ -269,12 +247,10 @@ def game_level(current_level, action, rAction, R1x, R1y, R2x, R2y, B1x, B1y, B2x
     Blue1Clicked = False
     Blue2Clicked = False
     
-    
     done = False
     mouse_pos = pygame.mouse.get_pos()
     mouse_x = mouse_pos[0]
     mouse_y = mouse_pos[1]
-    
     
     while not done:
         for event in pygame.event.get():
@@ -283,7 +259,7 @@ def game_level(current_level, action, rAction, R1x, R1y, R2x, R2y, B1x, B1y, B2x
                 pygame.quit()
                 quit()
                     
-                    #carry this on to check condition to be checked later for allowing to mouse to draw anywhere.
+            #carry this on to check condition to be checked later for allowing to mouse to draw anywhere.
                         
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 '''This stops program from crashing if user clicks anywhere but not the actual circle because line_colour
@@ -314,7 +290,6 @@ def game_level(current_level, action, rAction, R1x, R1y, R2x, R2y, B1x, B1y, B2x
                     GridPosition.pst()
                     #click_movement(mouse_x, mouse_y)
                     CheckPosition.pst()
-    
                     Connection.isConnected(orangegp1, orangegp2, redgp1, redgp2, yellowgp1, yellowgp2, greengp1, greengp2,
                     bluegp1, bluegp2)
                             
@@ -331,10 +306,6 @@ def game_level(current_level, action, rAction, R1x, R1y, R2x, R2y, B1x, B1y, B2x
                     # Bring up level complete screen if the current level is complete
                     LevelCompletion.situation("level complete", action, current_level)
                     
-                    
-                
-                
-         
         # Countdown timer: 
         Time.timer("out of time", action, current_level)
         Create.button("Reset Paths", 25,640,150,100, DarkBlue, Blue, 20, rAction)                  
@@ -343,9 +314,4 @@ def game_level(current_level, action, rAction, R1x, R1y, R2x, R2y, B1x, B1y, B2x
         # Update screen with changes
         pygame.display.flip()
 
-
-
 game_intro()
-    
-# Quit game
-pygame.quit()
